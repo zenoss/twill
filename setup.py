c95088a7d215f3486fe88ac5334c38e76c8da6fd
titus <titus@caltech.edu>
2006-01-17 19:01:36 -0600

    updated to a2
    darcs-hash:20060118010136-b517d-e470c170314edda69ff1ff78a538ef4bc4163810

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ use_setuptools()
 
 setup(name = 'twill',
       
-      version = '0.8.2a1',
+      version = '0.8.2a2',
       download_url = 'http://darcs.idyll.org/~t/projects/twill-0.8.1.tar.gz',
       
       description = 'twill Web browsing language',
