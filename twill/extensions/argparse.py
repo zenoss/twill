8d0ca9fcf1c397cd30df486e524fad4e08443d35
titus <titus@caltech.edu>
2006-04-29 21:44:12 -0500

    fixed misc, added 'require'
    darcs-hash:20060430024412-b517d-151df9aaffeecfcd693bdb81ce33ea6006903969

--- twill/extensions/argparse.py
+++ twill/extensions/argparse.py
@@ -3,18 +3,36 @@ Extension functions for parsing sys.argv.
 
 Commands:
 
-   get_args -- load all command-line arguments after -- into $arg0...$argN.
+   get_args -- load all command-line arguments after the last --
+      into $arg1...$argN.
    
 """
 
-def get_args():
-    import sys
-    from twill import commands, namespaces, shell
+import twill.utils
+
+def get_args(require=0):
+    """
+    >> get_args [<require>]
+
+    Load the command line arguments after the last '--' into $arg1...$argN,
+    optionally requiring at least 'require' such arguments.
+    """
+    from twill import commands, namespaces, shell, errors
 
     global_dict, local_dict = namespaces.get_twill_glocals()
 
-    for i, arg in enumerate(shell.twillargs):
-        global_dict["arg%d" % (i + 1,)] = arg
+    require = int(require)
 
-    print>>commands.OUT, "get_args: loaded %d args as $arg1..$arg%d." % \
-                         (i, i)
+    if len(shell.twillargs) < require:
+        from twill.errors import TwillAssertionError
+        raise TwillAssertionError("too few arguments; %d rather than %d" % \
+                                    (len(shell.twillargs), require,))
+
+    if shell.twillargs:
+        for i, arg in enumerate(shell.twillargs):
+            global_dict["arg%d" % (i + 1,)] = arg
+
+        print>>commands.OUT, "get_args: loaded %d args as $arg1..$arg%d." % \
+                             (i + 1, i + 1)
+    else:
+        print>>commands.OUT, "no arguments to parse!"
