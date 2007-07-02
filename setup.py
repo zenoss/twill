e85d88fe2e1aa109e8ceb01ebf43f1827ef459e1
titus <titus@idyll.org>
2007-07-02 14:38:31 -0500

    fixed setup.py wrt mechanize stuff
    darcs-hash:20070702193831-6d3ea-25c40cc18b50109ce674f2a0a42c4cea2557ef4b

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name = 'twill',
       license='MIT',
 
       packages = ['twill', 'twill.other_packages',
-                  'twill.other_packages.mechanize',
+                  'twill.other_packages._mechanize_dist',
                   'twill.extensions',
                   'twill.extensions.match_parse'],
 
