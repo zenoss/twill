22e155b09bfa9579dfa34efb6a01ecc255af48c2
Chet Luther <chet.luther@gmail.com>
2013-09-13 22:25:46 -0500

    Version 0.9 -> 0.9z1.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(name = 'twill',
       
-      version = '0.9',
+      version = '0.9z1',
       download_url = 'http://darcs.idyll.org/~t/projects/twill-0.9.tar.gz',
       
       description = 'twill Web browsing language',
